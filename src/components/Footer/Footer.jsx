c23e25a910bfed74d0780f8e2070e9dbc6f79a30
dylann97 <d.xavero@hotmail.com>
2024-04-15 14:03:17 +0200

    🚧 F01: App > Build the login pages

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center gap-10 pb-4 mt-10 z-30 text-threads-gray-light">
+      <Link href="#">© Threads</Link>
+      <Link href="#">Conditions générales de Threads</Link>
+      <Link href="#">Politique de confidentialité</Link>
+    </footer>
+  );
+}

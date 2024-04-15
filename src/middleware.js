f0b79c35a85cfb6d5375f78a747f2c74ab720dee
dylann97 <d.xavero@hotmail.com>
2024-04-15 14:02:02 +0200

    ✨ F01: middleware > Add code to check user by cookies in guest mode

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,19 @@
-import { Fascinate_Inline } from "next/font/google";
+import { hasCookie } from "cookies-next";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import React from "react";
 
 export function middleware(request) {
   let isAuthenticated = false;
 
+  // Check if user is guest
+  if (hasCookie("guest", { cookies })) {
+    isAuthenticated = true;
+  }
+
+  // Check if user is connected
+
+  // Check if user is not connected
   if (!isAuthenticated) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";

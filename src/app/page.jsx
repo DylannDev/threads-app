8cc381d0ee54891ada31edcb9b47d5400ca7e88d
dylann97 <d.xavero@hotmail.com>
2024-04-16 19:42:15 +0200

    🚧 F02: Home > build homepage with and add several threads

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,48 @@
-import AuthenticatedLayout from "@/components/AuthenticatedLayout/AuthenticatedLayout";
+import ConnectedLayout from "@/components/ConnectedLayout/ConnectedLayout";
 import Footer from "@/components/Footer/Footer";
+import Posts from "@/components/Posts/Posts";
 import React from "react";
 
 export default function Index() {
+  const posts = [
+    {
+      id: "1",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "John Doe",
+      picture: "/picture.png",
+    },
+    {
+      id: "2",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "John Doe",
+      picture: "/picture.png",
+    },
+    {
+      id: "3",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "John Doe",
+      picture: "/picture.png",
+    },
+    {
+      id: "4",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "John Doe",
+      picture: "/picture.png",
+    },
+    {
+      id: "5",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "John Doe",
+      picture: "/picture.png",
+    },
+  ];
   return (
-    <AuthenticatedLayout>
-      <div>accueil</div>
-    </AuthenticatedLayout>
+    <ConnectedLayout>
+      <div className="md:w-[700px] w-full mx-auto mt-10">
+        {posts.map((post) => (
+          <Posts key={post.id} post={post} />
+        ))}
+      </div>
+    </ConnectedLayout>
   );
 }

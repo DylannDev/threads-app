c00ca5d2a2bec5fbabbd4113963b2be87be40c11
dylann97 <d.xavero@hotmail.com>
2024-04-16 19:31:08 +0200

    🚧 F02: NavLinks > Create component to navigate in the app

---
+++ src/components/NavLinks/NavLinks.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { PiHouseFill, PiMagnifyingGlassBold } from "react-icons/pi";
+import { usePathname } from "next/navigation";
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex justify-center items-center w-full gap-5 absolute py-7">
+      <Link href="/">
+        <PiHouseFill
+          className={`${
+            pathname == "/" ? "text-white" : "text-threads-gray-light"
+          } w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl`}
+        />
+      </Link>
+      <Link href="/search">
+        <PiMagnifyingGlassBold
+          className={`${
+            pathname == "/search" ? "text-white" : "text-threads-gray-light"
+          } w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl`}
+        />
+      </Link>
+    </nav>
+  );
+}

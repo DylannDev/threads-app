15db2314ac7541032e942bc421ce35ab6c367823
dylann97 <d.xavero@hotmail.com>
2024-04-18 19:19:39 +0200

    💄 F04: Button > Add prop in Button component to disable button if no text typed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,12 +9,13 @@ export default function Button({
   className,
   noMarginTop,
   formButton,
+  disabled,
 }) {
   const { pending } = useFormStatus();
 
   return (
     <button
-      disabled={formButton && pending}
+      disabled={disabled || (formButton && pending)}
       onClick={onClick}
       className={`bg-white rounded-3xl border-threads-gray-light py-4 px-6 hover:bg-gray-300 duration-150 cursor-pointer disabled:bg-opacity-50 disabled:cursor-not-allowed ${className} ${
         !noMarginTop && "mt-4"

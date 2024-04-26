8e119203a0b9daceb9a189d22f25d277fe00c6b8
dylann97 <d.xavero@hotmail.com>
2024-04-26 17:12:09 +0200

    💄 F05: NavLinksMobile > Create new navbar component for mobile to make app more responsive

---
+++ src/components/NavLinksMobile/NavLinksMobile.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { PiHouseFill, PiMagnifyingGlassBold, PiUserBold } from "react-icons/pi";
+import { HiOutlinePencilAlt } from "react-icons/hi";
+import { usePathname } from "next/navigation";
+import { createPortal } from "react-dom";
+import NewPostForm from "../NewPostForm/NewPostForm";
+
+export default function NavLinksMobile({ session }) {
+  const pathname = usePathname();
+
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    if (openModal) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [openModal]);
+
+  return (
+    <div className="flex justify-center w-full fixed bottom-0 mb-5 z-50">
+      <nav className="flex sm:hidden justify-center items-center w-fit gap-5 py-4 px-8 border-[1px] border-threads-gray-light bg-threads-gray-dark rounded-full">
+        <Link href="/">
+          <PiHouseFill
+            className={`${
+              pathname == "/" ? "text-white" : "text-threads-gray-light"
+            } w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl`}
+          />
+        </Link>
+        <Link href="/search">
+          <PiMagnifyingGlassBold
+            className={`${
+              pathname == "/search" ? "text-white" : "text-threads-gray-light"
+            } w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl`}
+          />
+        </Link>
+        {session?.user?.email && (
+          <HiOutlinePencilAlt
+            onClick={() => setOpenModal(true)}
+            className={`w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl cursor-pointer text-threads-gray-light`}
+          />
+        )}
+        {session?.user?.email && (
+          <Link href={`/@${session.user.pseudo}`}>
+            <PiUserBold
+              className={`${
+                pathname.includes("@")
+                  ? "text-white"
+                  : "text-threads-gray-light"
+              } w-10 h-10 hover:bg-threads-gray-dark duration-150 p-1 rounded-xl`}
+            />
+          </Link>
+        )}
+      </nav>
+      {openModal &&
+        createPortal(
+          <div
+            className="modale-background"
+            onClick={(e) => {
+              if (e.target === e.currentTarget) {
+                setOpenModal(false);
+              }
+            }}
+          >
+            <div className="modale-foreground">
+              <NewPostForm closeModal={() => setOpenModal(false)} />
+            </div>
+          </div>,
+          document.body
+        )}
+    </div>
+  );
+}
